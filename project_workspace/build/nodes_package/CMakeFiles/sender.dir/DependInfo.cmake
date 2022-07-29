d6ab5a39270febcd8a1817958ac2b5746bfc4e4a
Alessio <alessiosal1998@gmail.com>
2022-07-29 14:35:37 +0200

    Added a simple node that sends the joint values to the topic

---
+++ project_workspace/build/nodes_package/CMakeFiles/sender.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alessio/RobotProgrammingProject/project_workspace/src/nodes_package/src/sender.cpp" "/home/alessio/RobotProgrammingProject/project_workspace/build/nodes_package/CMakeFiles/sender.dir/src/sender.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"nodes_package\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/ros/noetic/include"
+  "/opt/ros/noetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- project_workspace/build/nodes_package/catkin_generated/setup_cached.sh
+++ project_workspace/build/nodes_package/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/alessio/RobotProgrammingProject/project_workspace/devel/.private/nodes_package:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/alessio/RobotProgrammingProject/project_workspace/devel/.private/nodes_package/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/alessio/RobotProgrammingProject/project_workspace/devel/.private/nodes_package/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/alessio/RobotProgrammingProject/project_workspace/build/nodes_package'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/alessio/RobotProgrammingProject/project_workspace/devel/.private/nodes_package/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/alessio/RobotProgrammingProject/project_workspace/src/nodes_package:$ROS_PACKAGE_PATH"
