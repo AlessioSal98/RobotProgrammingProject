b310b7aa4fd9001fe6da639013b15c923f900ca9
Alessio <alessiosal1998@gmail.com>
2022-08-03 15:31:16 +0200

    Added workspace check

---
+++ project_workspace/build/nodes_package/CMakeFiles/vec_f_library.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/alessio/RobotProgrammingProject/project_workspace/devel/.private/nodes_package/lib/libvec_f_library.pdb"
+  "/home/alessio/RobotProgrammingProject/project_workspace/devel/.private/nodes_package/lib/libvec_f_library.so"
+  "CMakeFiles/vec_f_library.dir/src/vec_f.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vec_f_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
